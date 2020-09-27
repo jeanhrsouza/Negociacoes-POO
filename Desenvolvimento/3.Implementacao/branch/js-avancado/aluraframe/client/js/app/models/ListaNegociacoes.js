bc95b0a37da6f7290fee00f08dc85054edf85db9
Jean Souza <jeanhrsouza@hotmail.com>
2020-09-27 01:31:25 -0300

    arrow function e seu escopo lexico

--- Desenvolvimento/3.Implementacao/branch/js-avancado/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ Desenvolvimento/3.Implementacao/branch/js-avancado/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,15 @@
 class ListaNegociacoes {
     
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
         
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
     
     get negociacoes() {
@@ -20,8 +18,8 @@ class ListaNegociacoes {
     }
 
     esvazia() {
+
         this._negociacoes = [];
-        
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
 }
